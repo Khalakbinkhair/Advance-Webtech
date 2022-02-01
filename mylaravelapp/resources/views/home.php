04e648f2622e742242f7058d3acdeaea9d5fa7ab
Khalak bin Khair <Khalakbinkhair@gmail.com>
2022-02-01 13:04:13 +0600

    task1

---
+++ mylaravelapp/resources/views/home.php
@@ -0,0 +1,20 @@
+<html>
+<body bgcolor="grey">
+<form action="" method="">
+
+
+<a href="Login.php">Login</a>
+<a href="Home.php">Home</a>
+<a href="contact.php">Contact Us</a></br>
+
+<h3>Course: Programming With Python</h3>
+<h4>Instructor:Mr. David </br>
+Duration: 4 Months</br>
+Starts: 10th February 2022 </br></h4>
+Copy Right ©khalakbinkhair
+
+
+
+</form> 
+</body>
+</html>
